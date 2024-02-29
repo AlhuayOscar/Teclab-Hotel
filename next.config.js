7f9b6a65b5b344a6bf397ccaa6759747ee922348
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-02-29 17:38:13 -0300

    Routes for Api NextAuth and Hotel Lists

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
