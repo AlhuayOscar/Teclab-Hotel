82a427b222a9f73d872e5d7a93bfe72a203cc817
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-03-28 17:15:03 -0300

    Mailgun test 1

--- app/api/webhook/[webhook]/route.ts
+++ app/api/webhook/[webhook]/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
+import Mailgun from "mailgun.js";
+const mailgun = new Mailgun(FormData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY || "Key not found",
+});
 
 interface IParams {
   webhook?: string;
@@ -29,6 +35,16 @@ export async function POST(request: Request, { params }: { params: IParams }) {
         },
         data: paymentData,
       });
+      mg.messages
+        .create("sandbox-123.mailgun.org", {
+          from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+          to: ["oscar_alhuay2001@hotmail.com"],
+          subject: "Hello",
+          text: "Testing some Mailgun awesomness! Right?",
+          html: "<h1>Testing some Mailgun awesomness! Hey!!!</h1>",
+        })
+        .then((msg) => console.log(msg))
+        .catch((err) => console.error(err));
       return NextResponse.json({ reservation: updatedReservation });
     }
   }
