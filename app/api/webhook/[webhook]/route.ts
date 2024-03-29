98bdd06144f5894e9e71e7ebe8d79b0c949ce61c
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-03-28 22:46:45 -0300

    Online test 10

--- app/api/webhook/[webhook]/route.ts
+++ app/api/webhook/[webhook]/route.ts
@@ -21,6 +21,30 @@ export async function POST(request: Request, { params }: { params: IParams }) {
   const webhookId = params.webhook;
   const dateCreated = body.date_created;
   const paymentId = body.id;
+  const htmlContent = `
+    <img src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1709847291/rokqijqiwc0d4mms8ylo.jpg" style="width: 100%; max-width: 100%; max-height: 250px; overflow: hidden; object-fit: cover;" />
+    <div>
+        Your purchase is done! Feel free to read the description: 
+        <div style="max-width: 300px">
+        </div>
+    </div>
+`;
+
+  /*Si queremos agregar el email del usuario necesitamos 
+  poner en subject el valor de paymentBody.userEmail, 
+  En este caso no lo haremos porque es más facil el testeo*/
+
+  mg.messages
+    .create("sandbox249b93d991cd46279dda6eb7ef9af055.mailgun.org", {
+      from: "oscar_alhuay2001@hotmail.com",
+      to: ["bibarel9999@gmail.com"],
+      subject: "Test Mailgun",
+      text: "Testing some Mailgun awesomness! Right?",
+      html: htmlContent,
+    })
+    .then((msg) => console.log(msg))
+    .catch((err) => console.error(err));
+    
   if (paymentId) {
     const parsedBill = paymentId.toString();
     if (webhookId && body.action === "payment.created" && parsedBill) {
@@ -38,51 +62,7 @@ export async function POST(request: Request, { params }: { params: IParams }) {
     }
 
     console.log("Pago realizado");
-    const htmlContent = `
-    <img src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1709847291/rokqijqiwc0d4mms8ylo.jpg" style="width: 100%; max-width: 100%; max-height: 250px; overflow: hidden; object-fit: cover;" />
-    <div>
-        Your purchase is done! Feel free to read the description: 
 
-        <div style="max-width: 300px">
-        </div>
-
-        El id del pago realizado por Mercado Pago
-
-        <div> 
-        ${paymentId}
-        </div>
-
-
-        El id de la reserva pagada en HotelZZZ
-        <div> 
-        ${webhookId}
-        </div>
-      
-        <div>
-        <button>Quiero presentar una queja</button>
-        </div>
-
-        <div>
-        <button>Tuve un problema</button>
-        </div>
-      
-    </div>
-`;
-
-    /*Si queremos agregar el email del usuario necesitamos 
-  poner en subject el valor de paymentBody.userEmail, 
-  En este caso no lo haremos porque es más facil el testeo*/
-
-    mg.messages
-      .create("sandbox249b93d991cd46279dda6eb7ef9af055.mailgun.org", {
-        from: "oscar_alhuay2001@hotmail.com",
-        to: ["bibarel9999@gmail.com"],
-        subject: "Test Mailgun",
-        text: "Testing some Mailgun awesomness! Right?",
-        html: htmlContent,
-      })
-      .then((msg) => console.log(msg))
-      .catch((err) => console.error(err));
     return NextResponse.json(body);
   }
   return NextResponse.json("Webhook not working.");
