ee711b331ea9d7c744b0681d994c945ba665f54b
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-03-28 21:08:59 -0300

    Online test 8 Experimental / Revert

--- app/api/payment/[paymentid]/route.ts
+++ app/api/payment/[paymentid]/route.ts
@@ -40,8 +40,8 @@ export async function PUT(request: Request, { params }: { params: IParams }) {
   if (!reservationId || typeof reservationId !== "string") {
     throw new Error("Invalid ID");
   }
-        
-      const htmlContent = `
+
+  const htmlContent = `
     <img src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1709847291/rokqijqiwc0d4mms8ylo.jpg" style="width: 100%; max-width: 100%; max-height: 250px; overflow: hidden; object-fit: cover;" />
     <div>
         Your purchase is done! Feel free to read the description: 
@@ -72,21 +72,21 @@ export async function PUT(request: Request, { params }: { params: IParams }) {
     </div>
 `;
 
-      /*Si queremos agregar el email del usuario necesitamos 
+  /*Si queremos agregar el email del usuario necesitamos 
   poner en subject el valor de paymentBody.userEmail, 
   En este caso no lo haremos porque es más facil el testeo*/
 
-      mg.messages
-        .create("sandbox249b93d991cd46279dda6eb7ef9af055.mailgun.org", {
-          from: "oscar_alhuay2001@hotmail.com",
-          to: ["bibarel9999@gmail.com"],
-          subject: "Test Mailgun",
-          text: "Testing some Mailgun awesomness! Right?",
-          html: htmlContent,
-        })
-        .then((msg) => console.log(msg))
-        .catch((err) => console.error(err));
-    }
+  mg.messages
+    .create("sandbox249b93d991cd46279dda6eb7ef9af055.mailgun.org", {
+      from: "oscar_alhuay2001@hotmail.com",
+      to: ["bibarel9999@gmail.com"],
+      subject: "Test Mailgun",
+      text: "Testing some Mailgun awesomness! Right?",
+      html: htmlContent,
+    })
+    .then((msg) => console.log(msg))
+    .catch((err) => console.error(err));
+
   try {
     const result = await mercadopage.preferences.create({
       items: [
