57702c951f9ac03cceb8f90d8fd47287af7bc445
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2024-04-27 14:47:00 -0300

    Test Working Basic Search

--- e2e/basic-search.spec.ts
+++ e2e/basic-search.spec.ts
@@ -1,37 +1,61 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.locator(".p-3").first().click();
-  await page.getByText("🇦🇼Aruba,Americas").click();
+  await page.goto("https://teclab-hotel.vercel.app/en");
   await expect(
     page
       .locator("div")
-      .filter({ hasText: /^\+− Leaflet \| © OpenStreetMap contributors$/ })
+      .filter({ hasText: /^Hotel roomsBeach Housesin 1 click$/ })
       .first()
   ).toBeVisible();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hotel roomsBeach Housesin 1 click$/ })
+    .first()
+    .click();
+  await page.locator(".p-3").first().click();
+  await page.locator('div[role="option"]').first().click();
+await expect(
+  page
+    .locator("div")
+    .filter({
+      hasText: /.+/,
+    })
+    .first()
+).toBeVisible();
+  await page.getByRole("button", { name: "Marker" }).click();
   await page.getByRole("button", { name: "Next" }).click();
+  await expect(page.locator("button:nth-child(3)").first()).toBeVisible();
   await page.locator("button:nth-child(3)").first().click();
   await page.getByRole("button", { name: "Next" }).click();
-  await expect(
-    page
-      .locator("div")
-      .filter({ hasText: /^VisitorsHow many people are coming\?1$/ })
-      .getByRole("img")
-      .nth(1)
-  ).toBeVisible();
+  await expect(page.getByText("1", { exact: true }).first()).toBeVisible();
+  await page.getByText("1", { exact: true }).first().click();
+  await expect(page.locator("div:nth-child(3)").first()).toBeVisible();
+  await page.locator("div:nth-child(3)").first().click();
+  await expect(page.getByText("2", { exact: true })).toBeVisible();
   await page.getByText("2", { exact: true }).click();
+  await expect(page.getByText("1", { exact: true }).first()).toBeVisible();
+  await page.getByText("1", { exact: true }).first().click();
+  await expect(
+    page.locator("div:nth-child(4) > div:nth-child(2) > div:nth-child(3)")
+  ).toBeVisible();
   await page
     .locator("div:nth-child(4) > div:nth-child(2) > div:nth-child(3)")
     .click();
+  await expect(page.getByText("2", { exact: true }).nth(1)).toBeVisible();
   await page.getByText("2", { exact: true }).nth(1).click();
+  await expect(page.getByText("1", { exact: true })).toBeVisible();
+  await page.getByText("1", { exact: true }).click();
+  await expect(
+    page.locator("div:nth-child(6) > div:nth-child(2) > div:nth-child(3)")
+  ).toBeVisible();
   await page
     .locator("div:nth-child(6) > div:nth-child(2) > div:nth-child(3)")
     .click();
+  await expect(page.getByText("2", { exact: true }).nth(2)).toBeVisible();
   await page.getByText("2", { exact: true }).nth(2).click();
   await page.getByRole("button", { name: "Search" }).click();
-  await page
-    .locator("div")
-    .filter({ hasText: "Americas, ArubaCold Areas$" })
-    .nth(1)
-    .click();
+  await expect(
+    page.locator("div").filter({ hasText: "Americas, ArubaCold Areas$" }).nth(1)
+  ).toBeVisible();
 });
